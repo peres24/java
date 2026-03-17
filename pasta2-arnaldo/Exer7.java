ed5896a797ecba1a643b138a1a28a961b72811f4
peres24 <davidperesfilho24@gmail.com>
2026-03-17 07:38:02 -0300

    exer 7

---
+++ pasta2-arnaldo/Exer7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Exer7{
+    public static void main(String[]args){
+        Scanner ler = new Scanner(System.in);
+        int ano;
+        System.out.printf("Digite o ano atual:");
+        ano = ler.nextInt();
+        System.out.printf((ano%4==0) ? "ANO BISSEXTO!\n" :  "ANO NÃO BISSEXTO!\n");
+    }
+}
