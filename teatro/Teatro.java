68fe262abffb168c6a93c7e02282977b4d7c13c9
peres24 <davidperesfilho24@gmail.com>
2026-03-17 14:58:41 -0300

    Comeco do teatro

---
+++ teatro/Teatro.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Teatro{
+    public static void main(String[]args){
+        Scanner ler = new Scanner(System.in);
+        String  nome_teatro, nome_espetaculo;
+        int fileiras, cadeiras;
+        System.out.printf("     1   2   3   4   5   6   7   8   9   10  11  12\n");
+        System.out.printf("A   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("B   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("C   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("D   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("E   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("F   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("G   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("H   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("I   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("J   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("K   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        System.out.printf("L   []  []   []  []  []  []  []  []  []  []  []  []\n");
+        // System.out.printf("Digite o nome da sala do teatro: ");
+        // nome_teatro = ler.nextLine();
+        // System.out.printf("Digite o nome do espetaculo: ");
+        // nome_espetaculo = ler.nextLine();
+        // System.out.printf("Digite a quantidade de fileiras de cadeiras voce vai querer:");
+        // fileiras = ler.nextInt();
+        // if(fileiras > 12 || fileiras < 1){
+        //     while(fileiras > 12 || fileiras < 1){
+        //     System.out.printf("Voce pode ter no maximo 12 fileiras!\n");
+        //     System.out.printf("Digite a quantidade de fileiras de cadeiras voce vai querer:");
+        //     fileiras = ler.nextInt();
+        //     System.out.printf("\n");
+        //     }
+        // }
+        // System.out.printf("Digite a quantidade de cadeiras:");
+        // cadeiras = ler.nextInt();
+    }
+}
