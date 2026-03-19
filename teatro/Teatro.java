f06f1d737ce580c85db6ac3950badc8caaab1adc
peres24 <davidperesfilho24@gmail.com>
2026-03-19 20:11:02 -0300

    tava fazendo tudo errado, o usuario n é para digitar quant de sala nem de cadeira

--- teatro/Teatro.java
+++ teatro/Teatro.java
@@ -3,13 +3,13 @@ public class Teatro{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
         String  nome_sala, nome_espetaculo;
-        int fileiras, cadeiras, opcao, qfileiras = 0, qcadeiras = 0, i;
+        int fileiras, cadeiras, opcao, i, ingresso = 100, meiaingresso = 50;
         String[] letras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
         do{
         System.out.printf("------------------------------------\n");
         System.out.printf("          TEATRO DEVISATE           \n");
         System.out.printf("------------------------------------\n");
-        System.out.printf("1 - Cadastrar Sala\n");
+        System.out.printf("1 - Listar Salas\n");
         System.out.printf("2 - Exibir Mapa da Sala\n");
         System.out.printf("3 - Reservar Cadeira\n");
         System.out.printf("4 - Comprar Cadeira\n");
@@ -26,29 +26,19 @@ public class Teatro{
                 System.out.printf("Obrigado por usar nosso programa!\n");
                 break;
             case 1:
-                System.out.printf("Digite o nome da sala: ");
-                nome_sala = ler.nextLine();
-                System.out.printf("Digite o nome do espetáculo: ");
-                nome_espetaculo = ler.nextLine();
                 System.out.printf("\n");
-                System.out.printf("Digite a quantidade de fileiras que você quer:");
-                qfileiras = ler.nextInt();
-                System.out.printf("Digite a quantidade de cadeiras que você quer:");
-                qcadeiras = ler.nextInt();
-                if(qcadeiras > 12 || qfileiras > 12){
-                    System.out.printf("Erro - você pode ter no maximo 12 fileiras/cadeiras.\n");
-                }
+                System.out.printf("1 - Sala do Pequeno Principe\n2 - Sala do Pocoyo\n3 - Sala da Formiga e o Raposo\n");
                 break;
             case 2:
-                for (i = 1; i <= qcadeiras; i++){
+                for (i = 1; i <= 12; i++){
                     System.out.print("  "+i+" ");
                 }
                     System.out.printf("\n");
-                for(fileiras = 0; fileiras < qfileiras; fileiras++){
+                for(fileiras = 0; fileiras < 12; fileiras++){
                     for(i = 0; i < 1; i++){
                         System.out.printf(letras[fileiras]+" ");
                     }
-                    for(cadeiras = 1; cadeiras < qcadeiras; cadeiras++){
+                    for(cadeiras = 1; cadeiras < 12; cadeiras++){
                         System.out.printf("[]  ");
                     }
                     System.out.printf("[]\n");
@@ -58,21 +48,23 @@ public class Teatro{
         }while(opcao!=7);
 
 
-        // System.out.printf("Digite o nome da sala do teatro: ");
-        // nome_teatro = ler.nextLine();
-        // System.out.printf("Digite o nome do espetaculo: ");
-        // nome_espetaculo = ler.nextLine();
-        // System.out.printf("Digite a quantidade de fileiras de cadeiras voce vai querer:");
-        // fileiras = ler.nextInt();
-        // if(fileiras > 12 || fileiras < 1){
-        //     while(fileiras > 12 || fileiras < 1){
-        //     System.out.printf("Voce pode ter no maximo 12 fileiras!\n");
-        //     System.out.printf("Digite a quantidade de fileiras de cadeiras voce vai querer:");
-        //     fileiras = ler.nextInt();
-        //     System.out.printf("\n");
-        //     }
-        // }
-        // System.out.printf("Digite a quantidade de cadeiras:");
-        // cadeiras = ler.nextInt();
     }
 }
+
+
+// logica antiga para caso o usuario precise digitar a quantidade de cadeira ou fileira
+// for (i = 1; i <= qcadeiras; i++){
+//                     System.out.print("  "+i+" ");
+//                 }
+//                     System.out.printf("\n");
+//                 for(fileiras = 0; fileiras < qfileiras; fileiras++){
+//                     for(i = 0; i < 1; i++){
+//                         System.out.printf(letras[fileiras]+" ");
+//                     }
+//                     for(cadeiras = 1; cadeiras < qcadeiras; cadeiras++){
+//                         System.out.printf("[]  ");
+//                     }
+//                     System.out.printf("[]\n");
+//                 }
+//                 break;
+//         }
